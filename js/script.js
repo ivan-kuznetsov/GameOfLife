406f6b6d7e035de19491027d6ace8a2f6179fa93
Ivan Kuznetsov <Dizzy1986@yandex.ru>
2019-01-12 17:34:44 +0300

    implemented some things

--- js/script.js
+++ js/script.js
@@ -6,10 +6,25 @@ const GRID_COLS = 64;
 
 const GAME_SPEED = 100;
 
+const greed = createGrid(GRID_ROWS, GRID_COLS);
+const nextGrid = createGrid(GRID_ROWS, GRID_COLS);
+
 let isPlaying = false;
 
 const root = document.getElementById("root");
 const table = createTable(GRID_ROWS, GRID_COLS);
+createControls();
+
+function updateView() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            const cell = table.rows[i].cells[j];
+
+            const isCellAlive = grid[i][j];
+            cell.classList.toggle("alive", isCellAlive);
+        }
+    }
+}
 
 function createTable(rows, cols) {
     const table = document.createElement("table");
@@ -22,14 +37,10 @@ function createTable(rows, cols) {
 
         for (let j = 0; j < cols; j++) {
             const cell = document.createElement("td");
+
             cell.className = "cell";
             cell.width = GRID_WIDTH / cols;
             cell.height = GRID_HEIGHT / rows;
-
-            //   function handleClick() {
-            //     this.classList.toggle("alive");
-            //   }
-            //   cell.addEventListener("click", handleClick);
 
             row.appendChild(cell);
         }
@@ -43,10 +54,81 @@ function createTable(rows, cols) {
         }
 
         const cell = e.target;
-        cell.classList.toggle("alive");
+        const colIndex = cell.cellIndex;
+        const rowIndex = cell.parentNode.rowIndex;
+        const isCellAlive = grid[rowIndex][colIndex] === 1 ? true : false;
+
+        grid[rowIndex][cellIndex] = isCellAlive ? 0 : 1;
+
+        cell.classList.toggle("alive", !isCellAlive);
     });
 
     root.appendChild(table);
 
     return table;
+}
+
+function createControls() {
+    const startButton = document.createElement("button");
+    startButton.className = "material-icons";
+    startButton.textContent = "play_arrow";
+    startButton.addEventListener("click", function() {
+        if (isPlaying) {
+            isPlaying = false;
+            this.textContent = "play_arrow";
+        } else {
+            isPlaying = true;
+            this.textContent = "pause";
+        }
+    });
+
+    const resetButton = document.createElement("button");
+    resetButton.className = "material-icons";
+    resetButton.textContent = "replay";
+    resetButton.addEventListener("click", function() {
+        isPlaying = false;
+        startButton.textContent = "play_arrow";
+    });
+
+    const randomizeButton = document.createElement("button");
+    randomizeButton.className = "material-icons";
+    randomizeButton.textContent = "transform";
+    randomizeButton.addEventListener("click", function() {
+        isPlaying = false;
+        startButton.textContent = "play_arrow";
+
+        randomizeGrid();
+        updateView();
+    });
+
+    const container = document.createElement("div");
+    container.className = "controls";
+
+    container.append(startButton, resetButton, randomizeButton);
+
+    root.appendChild(container);
+}
+
+function createGrid(rows, cols) {
+    const grid = [];
+
+    for (let i = 0; i < rows; i++) {
+        grid[i] = [];
+
+        for (let j = 0; j < cols; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    return grid;
+}
+
+function randomizeGrid() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            grid[i][j] = Math.round(Math.random());
+        }
+    }
+
+    return grid;
 }
